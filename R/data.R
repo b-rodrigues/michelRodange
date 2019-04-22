a95f2a345def985c20fba7e4c6eacf8528d2af36
Bruno Rodrigues <bruno@brodrigues.co>
2019-04-22 09:41:20 +0200

    getting ready for cran release

---
+++ R/data.R
@@ -0,0 +1,43 @@
+#' Full text of the \emph{Renert} in Luxembourguish
+#'
+#' A dataset containing the full text of Michel Rodange's
+#' epic, \emph{Renert oder de Fuuß am Frack an a Ma'nsgrëßt} in
+#' Luxembourguish.
+#'
+#' @format A data frame with 6471 rows and 3 variables:
+#' \describe{
+#'   \item{\code{line}}{Original text}
+#'   \item{\code{gesank}}{\emph{Song}; the text is subdivided in songs, called \emph{gesank} in Luxembourguish}
+#'   \item{\code{nb_line}}{Line number}
+#' }
+#' @source \url{https://data.public.lu/fr/datasets/the-works-in-luxembourguish-of-michel-rodange/}
+"renert"
+
+#' Full text of the \emph{Sigfrid} play in Luxembourguish
+#'
+#' A dataset containing the full text of Michel Rodange's
+#' play, \emph{Dem Grow Sigfrid seng Goldkuommer} in
+#' Luxembourguish.
+#'
+#' @format A data frame with 6471 rows and 3 variables:
+#' \describe{
+#'   \item{\code{line}}{Original text}
+#'   \item{\code{act}}{Section of the play}
+#'   \item{\code{nb_line}}{Line number}
+#' }
+#' @source \url{https://data.public.lu/fr/datasets/the-works-in-luxembourguish-of-michel-rodange/}
+"sigfrid"
+
+#' Full text of the \emph{D'Léierchen} poem in Luxembourguish
+#'
+#' A dataset containing the full text of Michel Rodange's
+#' poem, \emph{D'Léierchen, Dem Léiweckerche säi Lidd} in
+#' Luxembourguish.
+#'
+#' @format A data frame with 6471 rows and 3 variables:
+#' \describe{
+#'   \item{\code{line}}{Original text}
+#'   \item{\code{nb_line}}{Line number}
+#' }
+#' @source \url{https://data.public.lu/fr/datasets/the-works-in-luxembourguish-of-michel-rodange/}
+"dleierchen"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -32,7 +32,7 @@ is_empty_character <- function(char){
 renert <- renert_songs %>%
     map(., ~discard(., is_empty_character)) %>%
     map(tibble) %>%
-    map(., ~rename(., sentence = `<chr>`)) %>%
+    map(., ~rename(., line = `<chr>`)) %>%
     map(., ~mutate(., gesank = pull(.[1, 1]))) %>%
     map(., ~mutate(., gesank = str_remove_all(gesank, " Gesank."))) %>%
     map(., ~mutate(., gesank = tolower(gesank))) %>%
@@ -59,3 +59,47 @@ sigfrid_raw <- read_lines(sigfrid_link)
 song_lines <- map2(indices_act, indices2_act,  ~seq(.x,.y))
 
 sigfrid_songs <- map(song_lines, ~`[`(sigfrid_raw, .))
+
+
+sigfrid <- sigfrid_songs %>%
+    map(., ~discard(., is_empty_character)) %>%
+    map(tibble) %>%
+    map(., ~rename(., line = `<chr>`)) %>%
+    map(., ~mutate(., act = pull(.[1, 1]))) %>%
+    map(., ~mutate(., act = str_remove_all(act, " ACT"))) %>%
+    map(., ~mutate(., act = str_remove_all(act, "\\."))) %>%
+    map(., ~mutate(., act = tolower(act))) %>%
+    bind_rows()
+
+sigfrid_intro <- sigfrid_raw[1:16] %>%
+    enframe(name = NULL) %>%
+    rename(line = value) %>%
+    mutate(act = "intro")
+
+sigfrid <- bind_rows(sigfrid_intro, sigfrid)
+
+nb_lines <- nrow(sigfrid)
+
+nrow_id <- seq(1, nb_lines)
+
+sigfrid$nb_line <- nrow_id
+
+usethis::use_data(sigfrid)
+
+
+
+
+dleierchen_link <- "https://download.data.public.lu/resources/the-works-in-luxembourguish-of-michel-rodange/20190414-194136/dleierchen.txt"
+
+dleierchen_raw <- read_lines(dleierchen_link)
+
+dleierchen <- dleierchen_raw %>% enframe(name = NULL) %>%
+    rename(line = value)
+
+nb_lines <- nrow(dleierchen)
+
+nrow_id <- seq(1, nb_lines)
+
+dleierchen$nb_line <- nrow_id
+
+usethis::use_data(dleierchen)
